16077e938a0b893b4827994cc420c28063fe6b37
Grndson <vukoedmund00@gmail.com>
2024-06-27 16:47:28 +0300

    Adding CAS-Final-Project files

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:cafeteriayetu@gmail.com">cafeteriayetu@gmail.com</a>
+         <a href="mailto:admincafeteria@gmail.com">admincafeteria@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>07:00am to 09:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Juja Kwale, Rd, Juja</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:0736940887">073-694-0887</a>
+         <a href="tel:0728048657">072-804-8657</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date(2024); ?> by <span> Edmund Vuko </span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
